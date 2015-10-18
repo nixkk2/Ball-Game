5429afa2bc7b159942b25581025595b3201e400a
BuildTools <unconfigured@null.spigotmc.org>
2015-10-17 23:47:27 -0700

    Fixed all

---
+++ Ball Game/Assets/Scripts/NewPlayer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewPlayer : MonoBehaviour {
+
+
+	private Rigidbody rb;
+    public Transform Spawn;
+    public NewPlayerController PC;
+    public float timer;
+    public bool timerB = true;
+
+    // Use this for initialization
+    void Start () {
+        Spawn = GameObject.Find("-Spawn").transform;
+        
+        rb = gameObject.GetComponent<Rigidbody> ();
+        transform.position = Spawn.position;
+
+    }
+
+	public void RES () {
+        Time.timeScale = 1;
+        Timer.timerB = false;
+        Timer.timer = 0;
+        PC.MaxBoostT = PC.BoostT;
+        //NewCameraController.YR = 
+		transform.position = Spawn.position;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        rb.freezeRotation = true;
+		rb.velocity = new Vector3 (0, 0, 0);
+		rb.freezeRotation = false;
+	}
+
+	void Update () {
+        if (timerB) {
+            timer += Time.deltaTime;
+        }
+        if (timer >= 0.05F && timerB) {
+            transform.position = Spawn.position;
+            timerB = false;
+        }
+        if (Input.GetButtonDown ("RS") && !EscMenu.menu) {
+			RES ();
+		}
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (transform.position.y < -10) {
+			RES ();
+		}
+	}
+}
---
+++ Ball Game/Assets/Scripts/Win.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Win : MonoBehaviour {
+
+    public Text HST;
+    public Text ST;
+    public GameObject winO;
+    public NewPlayer NP;
+    public static bool finish;
+
+    // Use this for initialization
+    void Start () {
+        finish = false;
+        winO.SetActive(false);
+	}
+	
+    public void Finish() {
+        Time.timeScale = 0;
+        finish = true;
+        NewPlayerController.Lock = false;
+        EscMenu.menu = true;
+        winO.SetActive(true);
+        if (Timer.timer < Timer.highTimer[Lvl.lvlName]) {
+            ST.text = "New high score: " + Timer.timer.ToString("#0.00");
+            float difference = Timer.highTimer[Lvl.lvlName] - Timer.timer;
+            HST.text = "Old high score: " + Timer.highTimer[Lvl.lvlName].ToString("#0.00") + ". You beat your old time by: " +  difference.ToString("#0.00");
+            Timer.highTimer[Lvl.lvlName] = Timer.timer;
+            PlayerPrefs.SetFloat(Lvl.lvlName, Timer.highTimer[Lvl.lvlName]);
+
+        }
+        else {
+            ST.text = "Score: " + Timer.timer.ToString("#0.00");
+            HST.text = "High score: " + Timer.highTimer[Lvl.lvlName].ToString("#0.00");
+        }
+
+
+    }
+
+    public void KP () {
+        finish = false;
+        NewPlayerController.Lock = true;
+        EscMenu.menu = false;
+        winO.SetActive(false);
+        NP.RES();
+    }
+    public void MM () {
+        
+        EscMenu.menu = false;
+        winO.SetActive(false);
+        Application.LoadLevel("TitleScreen");
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
