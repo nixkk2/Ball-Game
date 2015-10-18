67b305e61f976c12c2d68d442a3140d9e70935f3
BuildTools <unconfigured@null.spigotmc.org>
2015-10-18 12:06:29 -0700

    start

--- Ball Game/Assets/Scripts/Win.cs
+++ Ball Game/Assets/Scripts/Win.cs
@@ -4,58 +4,58 @@ using System.Collections;
 
 public class Win : MonoBehaviour {
 
-    public Text HST;
-    public Text ST;
-    public GameObject winO;
-    public NewPlayer NP;
-    public static bool finish;
+	public Text HST;
+	public Text ST;
+	public GameObject winO;
+	public NewPlayer NP;
+	public static bool finish;
 
-    // Use this for initialization
-    void Start () {
-        finish = false;
-        winO.SetActive(false);
+	// Use this for initialization
+	void Start () {
+		finish = false;
+		winO.SetActive(false);
 	}
 	
-    public void Finish() {
-        Time.timeScale = 0;
-        finish = true;
-        NewPlayerController.Lock = false;
-        EscMenu.menu = true;
-        winO.SetActive(true);
-        if (Timer.timer < Timer.highTimer[Lvl.lvlName]) {
-            PlayerPrefs.SetFloat("timer." + Lvl.lvlName, Timer.timer);
-            ST.text = "New high score: " + Timer.timer.ToString("#0.00");
-            float difference = Timer.highTimer[Lvl.lvlName] - Timer.timer;
-            HST.text = "Old high score: " + Timer.highTimer[Lvl.lvlName].ToString("#0.00") + ". You beat your old time by: " +  difference.ToString("#0.00");
-            Timer.highTimer[Lvl.lvlName] = Timer.timer;
-            
-            //print(Timer.highTimer[Lvl.lvlName]);
+	public void Finish() {
+		Time.timeScale = 0;
+		finish = true;
+		NewPlayerController.Lock = false;
+		EscMenu.menu = true;
+		winO.SetActive(true);
+		if (Timer.timer < Timer.highTimer[Lvl.lvlName]) {
+			PlayerPrefs.SetFloat("timer." + Lvl.lvlName, Timer.timer);
+			ST.text = "New high score: " + Timer.timer.ToString("#0.00");
+			float difference = Timer.highTimer[Lvl.lvlName] - Timer.timer;
+			HST.text = "Old high score: " + Timer.highTimer[Lvl.lvlName].ToString("#0.00") + ". You beat your old time by: " +  difference.ToString("#0.00");
+			Timer.highTimer[Lvl.lvlName] = Timer.timer;
+			
+			//print(Timer.highTimer[Lvl.lvlName]);
 
-        }
-        else {
-            ST.text = "Score: " + Timer.timer.ToString("#0.00");
-            HST.text = "High score: " + Timer.highTimer[Lvl.lvlName].ToString("#0.00");
-        }
+		}
+		else {
+			ST.text = "Score: " + Timer.timer.ToString("#0.00");
+			HST.text = "High score: " + Timer.highTimer[Lvl.lvlName].ToString("#0.00");
+		}
 
 
-    }
+	}
 
-    public void KP () {
-        finish = false;
-        NewPlayerController.Lock = true;
-        EscMenu.menu = false;
-        winO.SetActive(false);
-        NP.RES();
-    }
-    public void MM () {
-        
-        EscMenu.menu = false;
-        winO.SetActive(false);
-        Application.LoadLevel("TitleScreen");
-    }
+	public void KP () {
+		finish = false;
+		NewPlayerController.Lock = true;
+		EscMenu.menu = false;
+		winO.SetActive(false);
+		NP.RES();
+	}
+	public void MM () {
+		
+		EscMenu.menu = false;
+		winO.SetActive(false);
+		Application.LoadLevel("TitleScreen");
+	}
 
 	// Update is called once per frame
 	void Update () {
-	    
+		
 	}
 }
